ec5b4c72d48da67d481534c0b89a80fab9187506
lhxo2 <79862589+lhxo2@users.noreply.github.com>
2021-03-01 08:55:42 -0800

    Update tempconversion.php

--- tempconversion.php
+++ tempconversion.php
@@ -1,6 +1,65 @@
 <!--tempconversion.php-->
 <?php
+        if(isset($_POST["tTemperature"]) && isset($_POST["rRadio"])){
 
+                //Defining Variables
+                $inputtedTemperature = $_POST["tTemperature"]; // This is the number the user inputs
+                $inputtedRadio = $_POST["rRadio"]; // this is the temperature type chosen
+                $inputConcat="$inputtedTemperature$inputtedRadio";
+                $results1=0; //Variable to store results in
+                $results2=0; //Variables to store results in
+                $D = '&#176'; //This is a string for degree symbol
+                $K = '&#8490'; //K symbol
+                $F = 'F';
+                $C = 'C';
+
+                //Input checker & Calculations
+                //Check's what kind radio button you inputted and does the specific calculations.
+                if($_POST["rRadio"]=='F'){    //Fahrenheit Calculations
+                        //Fahrenheit to Celsius
+                        $FtoC = (($inputtedTemperature - 32) * (5/9));
+                        //Fahrenheit to Kelvin
+                        $FtoK = (($inputtedTemperature - 32) * (5/9) + 273.15);
+                } else if($_POST["rRadio"]=='C') {   //Celsius Calculations
+                        //Celsius to Fahrenheit
+                        $CtoF = ($inputtedTemperature * (9/5) + 32);
+                        //Celsius to Kelvin
+                        $CtoK = ($inputtedTemperature + 273.15);
+                } else if($_POST["rRadio"]=='K') {    //Kelvin Calculations
+                        //Kelvin to Fahrenheit
+                        $KtoF = ($inputtedTemperature - 273.15) * (9/5) + 32;
+                        //Kelvin to Celsius
+                        $KtoC = ($inputtedTemperature - 273.15);
+                }
+                
+                //Results
+                //Checks the inputted radio button and stores the results
+                if($_POST["rRadio"]=='F'){
+                        $results1 = $FtoC;
+                        $results2 = $FtoK;
+                } else if($_POST["rRadio"]=='C') {
+                        $results1 = $CtoF;
+                        $results2 = $CtoK;
+                } else if($_POST["rRadio"]=='K') {
+                        $results1 = $KtoF;
+                        $results2 = $KtoC;
+                }
+
+                //Formatting
+                //Grabs the result and formats to X.00 + the Radio type + Degree
+                if($_POST["rRadio"]=='F'){
+                        $results1 = number_format($results1, 2);
+                        $results2 = number_format($results2, 2);
+                } else if($_POST["rRadio"]=='C') {
+                        $results1 = number_format($results1, 2);
+                        $results2 = number_format($results2, 2);
+                } else if($_POST["rRadio"]=='K') {
+                        $results1 = number_format($results1, 2);
+                        $results2 = number_format($results2, 2);
+                }
+        }
+        
+}
 ?>
 
 <!DOCTYPE html>
