021635b15afb06a37b07df09eb90709645ef6c1c
lhxo2 <79862589+lhxo2@users.noreply.github.com>
2021-03-01 08:49:58 -0800

    Update tempconversion.php

--- tempconversion.php
+++ tempconversion.php
@@ -5,5 +5,26 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
+    <head>
+        <title>P1 - Temperature Conversion</title>
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="viewport" content="width=device-width" />
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="temp.css"/>
+    </head>
+    <body>
+        <h1>Temperature Converter</h1>
+        <form action="" method="POST">
+            <p>Temperature to Convert: <input type="text" name="tTemperature" /></p>
+            <!--Error message appears clear until validation-->
+            <p class="error-message"><?php echo $errorMessage?></p>
+            <fieldset>
+                <legend>Please Select A Conversion Type</legend>
+                <p><input type="radio" name="rRadio" value="F" />Fahrenheit</p>
+                <p><input type="radio" name="rRadio" value="C" />Celsius</p>
+                <p><input type="radio" name="rRadio" value="K" />Kelvin</p>
+            </fieldset>
+            <p><input type="submit" value="Convert"/></p>
+        </form>
+    </body>
 </html>
